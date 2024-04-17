5122f4bbe264426a115704902f3a4f1ba8609489
Daniil Utkin <d.utkin@tesonero.com>
2024-04-18 01:00:12 +0300

    add mock response fetch time

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,7 +8,14 @@ export async function fetchRevenue() {
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
 
   try {
-    return (await fetch('http://localhost:3001/revenue')).json();
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    const response = (await fetch('http://localhost:3001/revenue')).json();
+    console.log('Data fetch completed after 3 seconds.');
+
+    return response;
+
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
